5334e3cb8a25ac30b5e2a5026696e272357e1b97
Ash Ajida <ashraf.ajida@remarkable.com>
2024-07-08 17:59:00 +0100

    fix build error

--- gamble-frontend/src/app/studio/studio.tsx
+++ gamble-frontend/src/app/studio/studio.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { NextStudio } from "next-sanity/studio"
-import sanityConfig from "../../../../../sanity.config"
+import sanityConfig from "../../../sanity.config"
 
 const Studio = () => {
     return <NextStudio  config={sanityConfig} />
